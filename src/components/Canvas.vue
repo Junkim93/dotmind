05eecec2fc09292c5dcd689a8416cecb3519115d
Jun Kim <93kimhyunjun@gmail.com>
2019-10-07 22:48:19 +0900

    Add a feature: paint pixels

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="canvas">
     <h1>{{ msg }}</h1>
-    <table class="pixel-canvas"></table>
+    <table
+      class="pixel-canvas"
+      @mousedown="isTrueDrawing"
+      @mouseleave="isFalseDrawing"
+      @mouseup="isFalseDrawing"
+      @mouseover="paintPixel"
+    ></table>
+
+    <div class="palette">
+      <swatches v-model="currentColor" />
+    </div>
   </div>
 </template>
 
 <script>
+import Swatches from "vue-swatches";
+import "vue-swatches/dist/vue-swatches.min.css";
+
 export default {
   name: "Canvas",
   props: {
     msg: String
   },
+  components: { Swatches },
   data() {
     return {
       pixelWidth: 32,
-      pixelHeight: 32
+      pixelHeight: 32,
+      isDrawing: false,
+      currentColor: "black"
     };
   },
   methods: {
@@ -27,9 +43,19 @@ export default {
         for (let j = 0; j < this.pixelWidth; j += 1) {
           const gridCell = document.createElement("td");
           gridRow.appendChild(gridCell);
-          console.log(i, j);
         }
       }
+    },
+    isTrueDrawing(e) {
+      this.isDrawing = true;
+      this.paintPixel(e);
+    },
+    isFalseDrawing() {
+      this.isDrawing = false;
+    },
+    paintPixel(e) {
+      if (this.isDrawing && e.target.tagName === "TD")
+        e.target.style.backgroundColor = this.currentColor;
     }
   },
   mounted() {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
     <Canvas msg="DOTMIND" />
-    <Palette />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Canvas from "@/components/Canvas";
-import Palette from "@/components/Palette";
 
 export default {
   name: "home",
   components: {
-    Canvas,
-    Palette
+    Canvas
   }
 };
 </script>
