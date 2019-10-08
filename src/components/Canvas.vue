eaff54d29c26cc11b11af7c6d4bc60d06c5eef60
Jun Kim <93kimhyunjun@gmail.com>
2019-10-08 22:27:54 +0900

    Add: Add to 16 color palette and eraser

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -10,17 +10,18 @@
     ></table>
 
     <div class="palette">
-      <swatches v-model="currentColor" />
+      <swatches v-model="currentColor" inline />
+      <button class="palette-eraser" @click.prevent="eraseAllPalette">CLEAR</button>
     </div>
   </div>
 </template>
 
 <script>
-import Swatches from "vue-swatches";
-import "vue-swatches/dist/vue-swatches.min.css";
+import Swatches from 'vue-swatches'
+import 'vue-swatches/dist/vue-swatches.min.css'
 
 export default {
-  name: "Canvas",
+  name: 'Canvas',
   props: {
     msg: String
   },
@@ -30,38 +31,42 @@ export default {
       pixelWidth: 32,
       pixelHeight: 32,
       isDrawing: false,
-      currentColor: "black"
-    };
+      currentColor: 'black'
+    }
   },
   methods: {
     makeGrid() {
-      const pixelCanvas = document.querySelector(".pixel-canvas");
+      const pixelCanvas = document.querySelector('.pixel-canvas')
       for (let i = 0; i < this.pixelHeight; i += 1) {
-        const gridRow = document.createElement("tr");
-        pixelCanvas.appendChild(gridRow);
+        const gridRow = document.createElement('tr')
+        pixelCanvas.appendChild(gridRow)
 
         for (let j = 0; j < this.pixelWidth; j += 1) {
-          const gridCell = document.createElement("td");
-          gridRow.appendChild(gridCell);
+          const gridCell = document.createElement('td')
+          gridRow.appendChild(gridCell)
         }
       }
     },
     isTrueDrawing(e) {
-      this.isDrawing = true;
-      this.paintPixel(e);
+      this.isDrawing = true
+      this.paintPixel(e)
     },
     isFalseDrawing() {
-      this.isDrawing = false;
+      this.isDrawing = false
     },
     paintPixel(e) {
-      if (this.isDrawing && e.target.tagName === "TD")
-        e.target.style.backgroundColor = this.currentColor;
+      if (this.isDrawing && e.target.tagName === 'TD')
+        e.target.style.backgroundColor = this.currentColor
+    },
+    eraseAllPalette(e) {
+      const pixelCanvas = document.querySelector('.pixel-canvas')
+      pixelCanvas.querySelectorAll('td').forEach(td => (td.style.backgroundColor = 'white'))
     }
   },
   mounted() {
-    this.makeGrid();
+    this.makeGrid()
   }
-};
+}
 </script>
 
 <style scoped>
