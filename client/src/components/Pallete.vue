a80513d346f031b7708eb4e0ace834a032163495
Jun Kim <93kimhyunjun@gmail.com>
2020-01-24 15:20:14 +0900

    add pallete component

--- client/src/components/Pallete.vue
+++ client/src/components/Pallete.vue
@@ -1,10 +1,38 @@
-<template></template>
+<template>
+  <div class="wrapper">
+    <div class="wrapper" v-for="(item, index) in color" :key="index">
+      <button class="test" :style="{ backgroundColor: item }" @click.prevent="emitColorInfo(item)"></button>
+    </div>
+  </div>
+</template>
 
 <script>
-import Swatches from 'vue-swatches';
-import 'vue-swatches/dist/vue-swatches.min.css';
+export default {
+  data() {
+    return {
+      color: ['red', 'black', 'yellow'],
+      currentColor: ''
+    };
+  },
 
-export default {};
+  methods: {
+    emitColorInfo(item) {
+      this.currentColor = item;
+      this.$emit('update-color', this.currentColor);
+    }
+  }
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+}
+.test {
+  width: 40px;
+  height: 40px;
+  margin: 5px;
+}
+</style>
