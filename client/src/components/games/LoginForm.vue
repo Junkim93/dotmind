6b7ceffb995f733681ae38f8a418a9bda24b741c
Jun Kim <93kimhyunjun@gmail.com>
2020-02-20 23:35:42 +0900

    기타 파일 초기화

---
+++ client/src/components/games/LoginForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="nes-field">
+      <label for="name_field">Your name</label>
+      <input type="text" id="name_field" class="nes-input" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ client/src/components/layouts/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <router-link to="/">도트 마인드</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  font-size: 40px;
+  padding: 30px;
+  text-align: center;
+
+  > a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
