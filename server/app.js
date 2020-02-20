79a1fe0080e9e6b6be930c2694322e0af0d9723c
Jun Kim <93kimhyunjun@gmail.com>
2020-02-21 08:38:46 +0900

    Heroku test

--- server/app.js
+++ server/app.js
@@ -13,10 +13,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(history())
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, './public')))
 
 app.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+  res.sendFile(path.join(__dirname, './public', 'index.html'))
 })
 
 module.exports = app
