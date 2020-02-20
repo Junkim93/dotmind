8045a9db5663389f50e26f058145056d78b39a6a
Jun Kim <93kimhyunjun@gmail.com>
2020-02-20 23:35:09 +0900

    Canvas & Pallete 컴포넌트 재작성 및 로직 성능 개선

---
+++ client/src/components/paintings/Canvas.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="canvas">
+    <div class="canvas__wrapper" ref="canvasImage">
+      <div
+        class="canvas__wrapper__column"
+        v-for="(column, columnKey) in pixelData"
+        :key="`column${columnKey}`"
+      >
+        <span
+          class="canvas__wrapper__row"
+          v-for="(row, rowKey) in column"
+          :key="`row${rowKey}`"
+          :id="row.id"
+          :style="{ backgroundColor: row.bgColor }"
+          @mousedown.prevent="startDrawing"
+          @mousemove.prevent="keepDrawing"
+          @mouseup.prevent="endDrawing"
+        ></span>
+      </div>
+    </div>
+    <Pallete @update-color="changeColor" />
+    <div class="canvas__utils">
+      <button class="nes-btn is-error canvas__utils__clear" @click="clearPaint">
+        모두 지우기
+      </button>
+      <button class="nes-btn canvas__utils__download" @click="downloadImg">
+        그림 다운로드
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+import domtoimage from 'dom-to-image';
+import download from 'downloadjs';
+import Pallete from './Pallete';
+
+const socket = io('http://localhost:4000/multi-game');
+
+export default {
+  components: { Pallete },
+
+  props: {
+    multiMode: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      framePixel: 16,
+      pixelData: [],
+      isDrawing: false,
+      currentColor: 'black',
+    };
+  },
+
+  created() {
+    this.initPixelData();
+    this.setPixelData();
+  },
+
+  mounted() {
+    this.receivePaint();
+  },
+
+  methods: {
+    initPixelData() {
+      const newArr = Array.apply(null, Array(this.framePixel));
+      this.pixelData = newArr.map(el => {
+        el = newArr;
+        return el;
+      });
+    },
+    setPixelData() {
+      this.pixelData = this.pixelData.map((column, columnIndex) => {
+        return column.reduce((acc, row, rowIndex) => {
+          acc.push({ id: `${columnIndex}+${rowIndex}`, bgColor: '' });
+          return acc;
+        }, []);
+      });
+    },
+
+    paintPixel(v) {
+      const targetPixel = v.toElement.id.split('+');
+      const columnKey = targetPixel[0];
+      const rowKey = targetPixel[1];
+
+      this.pixelData[columnKey][rowKey].bgColor = this.currentColor;
+
+      if (this.multiMode) {
+        this.sendPaint({
+          columnKey: columnKey,
+          rowKey: rowKey,
+          color: this.currentColor,
+        });
+      }
+    },
+
+    sendPaint(targetInfo) {
+      socket.emit('paintInfo', targetInfo);
+    },
+
+    receivePaint() {
+      socket.on('receivePaintInfo', target => {
+        this.pixelData[target.columnKey][target.rowKey].bgColor = target.color;
+      });
+    },
+
+    startDrawing(v) {
+      this.isDrawing = true;
+      this.paintPixel(v);
+    },
+
+    keepDrawing(v) {
+      if (this.isDrawing) {
+        this.paintPixel(v);
+      }
+    },
+
+    endDrawing() {
+      this.isDrawing = false;
+    },
+
+    changeColor(v) {
+      this.currentColor = v;
+    },
+
+    async downloadImg() {
+      const node = this.$refs.canvasImage;
+      const blob = await domtoimage.toBlob(node);
+      download(blob, 'undefined.png', 'image/png');
+    },
+
+    clearPaint() {
+      for (let i = 0; i < this.pixelData.length; i += 1) {
+        for (let j = 0; j < this.pixelData.length; j += 1) {
+          this.pixelData[i][j].bgColor = '';
+        }
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.canvas {
+  padding: 0 30px;
+
+  &__wrapper {
+    background-image: linear-gradient(
+        45deg,
+        #e2e2e2 25%,
+        transparent 25%,
+        transparent 75%,
+        #e2e2e2 75%,
+        #e2e2e2
+      ),
+      linear-gradient(
+        45deg,
+        #e2e2e2 25%,
+        transparent 25%,
+        transparent 75%,
+        #e2e2e2 75%,
+        #e2e2e2
+      );
+    background-color: #fdfdfd;
+    background-size: 30px 30px;
+    background-position: 0 0, 15px 15px;
+
+    border: 2px solid rgb(97, 97, 97);
+
+    &__column {
+      width: 100%;
+      height: 30px;
+    }
+    &__row {
+      display: inline-block;
+      width: 30px;
+      height: 30px;
+      border: 1px solid rgba(105, 105, 105, 0.333);
+
+      &:hover {
+        background-color: #efed97;
+      }
+    }
+  }
+  &__utils {
+    display: flex;
+    justify-content: center;
+    margin: 20px 0;
+
+    &__download,
+    &__clear {
+      margin-left: 20px;
+      margin-right: 20px;
+    }
+  }
+}
+</style>
---
+++ client/src/components/paintings/Pallete.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="pallete">
+    <div class="pallete-wrapper" v-for="(item, index) in color" :key="index">
+      <button
+        class="nes-btn pallete-wrapper__paint"
+        :style="{ backgroundColor: item }"
+        @click.prevent="emitColor(item)"
+      />
+    </div>
+    <button
+      class="nes-btn pallete-wrapper__eraser"
+      @click.prevent="emitColor('')"
+    >
+      지우개
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      color: [
+        '#ffffff',
+        '#000000',
+        '#fe0000',
+        '#fd7500',
+        '#FEF400',
+        '#02fe00',
+        '#0595FF',
+        '#D201FD',
+      ],
+      currentColor: '',
+    };
+  },
+
+  methods: {
+    emitColor(item) {
+      this.currentColor = item;
+      this.$emit('update-color', this.currentColor);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.pallete {
+  display: flex;
+  justify-content: center;
+  margin: 10px 0;
+
+  &-wrapper {
+    &__paint,
+    &__eraser {
+      width: 35px;
+      height: 35px;
+      margin: 5px;
+    }
+    &__eraser {
+      width: 70px;
+      font-size: 15px;
+      text-align: center;
+    }
+  }
+}
+</style>
